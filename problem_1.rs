2c7ac8a4d00e352fb4c1c28be1246b1762ee48f4
DavidDo <dungdo.hust@gmail.com>
2022-06-12 22:40:14 +0900

    Add files via upload

---
+++ problem_1.rs
@@ -0,0 +1,20 @@
+// problem 1: checking sub array
+
+fn main() {
+    let org_arr = [1,2,3,5,6,8,10,11];
+    let sub_arr = [6,8,15];
+    let mut count: i32 = 0;
+    for i in org_arr{
+        for j in sub_arr {
+            if i == j {
+                count += 1;
+            }
+        }
+    }
+    if count == (sub_arr.len() as i32) {
+        println!("sub array found!");
+    }
+    else {
+        println!("not matched!")
+    }
+}
