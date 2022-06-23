29fca90f560bda41b0fc3e1ef0a9b1c46648d887
Do <dungdo.hust@gmail.com>
2022-06-23 15:23:39 +0900

    workshop 4 lifetime error

--- workshop_4/src/main.rs
+++ workshop_4/src/main.rs
@@ -1,28 +1,51 @@
 
-#[derive(Debug)]
-struct Fibonacci {
-    a: u32,
-    b: u32,
-}
-fn fibonacci_number () -> Fibonacci {
-    Fibonacci { a: 0, b: 1 }
-}
+// Problem 1: fibonnaci number
 
-impl Iterator for Fibonacci {
-    type Item = u32;
-    fn next(&mut self) -> Option<Self::Item> {
-        let new_b = self.a + self.b;
+// #[derive(Debug)]
+// struct Fibonacci {
+//     a: u32,
+//     b: u32,
+// }
+// fn fibonacci_number () -> Fibonacci {
+//     Fibonacci { a: 0, b: 1 }
+// }
 
-        self.a = self.b;
-        self.b = new_b;
+// impl Iterator for Fibonacci {
+//     type Item = u32;
+//     fn next(&mut self) -> Option<Self::Item> {
+//         let new_b = self.a + self.b;
 
-        Some(self.a)
-    }
+//         self.a = self.b;
+//         self.b = new_b;
+
+//         Some(self.a)
+//     }
     
-}
+// }
+// fn main() {
+//     for i in fibonacci_number().take(10) {
+//         println!("> {}", i);
+//     }
+    
+// }
+
+//Problem 2: Lifetime error
+
+use std::fmt;
+struct StrDisplayable<'a>(Vec<&'a str>);
+
+impl fmt::Display for StrDisplayable<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for v in &self.0 {
+            write!(f, "\n{}", v)?;
+
+        }
+        Ok(())
+    }
+} 
+
 fn main() {
-    for i in fibonacci_number().take(10) {
-        println!("> {}", i);
-    }
-    
+    let vec: Vec<&str> = vec!["a", "bc", "def"];
+    let vec_foo = StrDisplayable(vec);
+    println!("{}", vec_foo);
 }
